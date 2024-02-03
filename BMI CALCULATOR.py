151ed4b72b50aec7b6670c604532616dd2571dca
Kabirroyy <158730229+Kabirroyy@users.noreply.github.com>
2024-02-03 23:59:37 +0530

    Add files via upload

---
+++ BMI CALCULATOR.py
@@ -0,0 +1,45 @@
+
+#AUTHOR-KABIR ROY
+
+#BMI CALCULATOR (APPROACH-1)
+
+# height=float(input("Enter your height in m: "))
+
+
+# weight=float(input("Enter your weight in kg: "))
+
+
+# BMI=weight/height**2
+
+
+# print(float(BMI))
+
+# **=Exponent
+# WE SHOULD KNOW THE FORMULA OF BMI.
+# WE ARE USING FLOAT TO GET THE EXACT APPROACH.
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#BMI CALCULATOR(APPROACH-2)
+
+height=float(input("What is your height:"))
+
+weight=float(input("What is your weight"))                                  
+
+if weight<18.5:
+  print("You are underweight")
+elif 18.5<weight<25:
+  print("You have normal weight")
+elif 25<weight<30:
+  print("You are obese")
+else:
+  print("You are clinically obese")
+  
+bmi=weight/height**2
+
+print(f'Your bmi is {bmi}')
+
+
+# Used if - elif - else loop
+# Usage of s string is better for converting int to str directly rather than using str()
+
+# --------------------------------------------------------------------------------------------------------------------------------------
